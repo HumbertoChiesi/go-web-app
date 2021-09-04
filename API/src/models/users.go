a99e3e1cf4069c873f451a20d6c4ef1ab9e7ceec
HumbertoChiesi <66853228+HumbertoChiesi@users.noreply.github.com>
2021-09-03 23:53:51 -0300

    user validation

--- API/src/models/users.go
+++ API/src/models/users.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //User represents an user using the social media
 type User struct {
@@ -10,4 +14,36 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedOn time.Time `json:"createdOn,omitempty"`
+}
+
+//Prepare will call the methods to validate and format the received user
+func (user *User) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+	user.format()
+
+	return nil
+}
+
+func (user *User) validate() error {
+	if user.Name == "" {
+		return errors.New("the name field cannot be empty")
+	}
+
+	if user.Nick == "" {
+		return errors.New("the nick field cannot be empty")
+	}
+
+	if user.Email == "" {
+		return errors.New("the email field cannot be empty")
+	}
+
+	return nil
+}
+
+func (user *User) format() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
